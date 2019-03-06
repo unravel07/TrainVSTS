1d107a65e93301e28c60cb661a154eab5fafcc8e
unravel07 <mick_ben10@hotmail.com>
2019-03-06 16:07:22 +0700

    model1

---
+++ NSOVarTestAPI/Models/model1.cs
@@ -0,0 +1,21 @@
+namespace NSOVarTestAPI.Models
+{
+    public class model1
+    {
+        public string _id { get; set; }
+        public bool STATUS { get; set; }
+        public string CWT { get; set; }
+        public string CWT_NAME { get; set; }
+        public string TID { get; set; }
+        public string TYPE_NAME { get; set; }
+        public string USERID { get; set; }
+        public string TITLE { get; set; }
+        public string FIRSTNAME { get; set; }
+        public string LASTNAME { get; set; }
+        public string PHONE { get; set; }
+        public string SSN { get; set; }
+        public string EMAIL { get; set; }
+        public string PASSWORD { get; set; }
+        public int __v { get; set; }
+    }
+}
